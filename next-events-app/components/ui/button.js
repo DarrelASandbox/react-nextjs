e63002afb2781e10dace5d67b6bfa130368d8fc1
DarrelA <darrelaiscoding@gmail.com>
2023-04-02 21:26:32 +0800

    Event search component

--- next-events-app/components/ui/button.js
+++ next-events-app/components/ui/button.js
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import classes from './button.module.css';
 
-const Button = (props) => (
-  <Link href={props.link} className={classes.btn}>
-    {props.children}
-  </Link>
-);
-
+const Button = (props) =>
+  props.link ? (
+    <Link href={props.link} className={classes.btn}>
+      {props.children}
+    </Link>
+  ) : (
+    <button onClick={props.onClick} className={classes.btn}>
+      {props.children}
+    </button>
+  );
 export default Button;
--- next-events-app/pages/events/index.js
+++ next-events-app/pages/events/index.js
@@ -1,13 +1,21 @@
 import EventList from '@/components/events/event-list';
+import EventSearch from '@/components/events/event-search';
 import { getAllEvents } from '@/dummy-data';
+import { useRouter } from 'next/router';
 
 const AllEventsPage = () => {
+  const router = useRouter();
   const allEvents = getAllEvents();
 
+  const findEventsHandler = (year, month) => {
+    router.push(`/events/${year}/${month}`);
+  };
+
   return (
-    <div>
+    <>
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={allEvents} />
-    </div>
+    </>
   );
 };
 
