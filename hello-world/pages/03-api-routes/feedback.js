91dbee0e883ccd7645db3ee81d2250cf995d7efc
DarrelA <darrelaiscoding@gmail.com>
2023-04-22 22:08:25 +0800

    Dynamic API route

--- hello-world/pages/03-api-routes/feedback.js
+++ hello-world/pages/03-api-routes/feedback.js
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import { buildFeedbackPath, extractFeedback } from '../api/feedback';
 
 const feedbackPage = (props) => {
+  const [feedbackData, setFeedbackData] = useState();
+
+  // Redundant because we have already received the full data via props
+  // however, this is how dynamic API route feature is used
+  const loadFeedbackHandler = async (id) => {
+    const response = await fetch(`/api/${id}`);
+    const data = await response.json();
+    setFeedbackData(data.feedback);
+  };
+
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>{item.feedbackText}</li>
-      ))}
-    </ul>
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.feedbackText}
+            <button onClick={() => loadFeedbackHandler(item.id)}>Show Details</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
