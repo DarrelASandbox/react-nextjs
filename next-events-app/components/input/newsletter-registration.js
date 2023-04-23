fbc1fb3b67b49278f6e6f6dfcb39bbafd4dfc0ab
DarrelA <darrelaiscoding@gmail.com>
2023-04-23 12:43:15 +0800

    `POST` request for email input

--- next-events-app/components/input/newsletter-registration.js
+++ next-events-app/components/input/newsletter-registration.js
@@ -1,13 +1,21 @@
+import { useRef } from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
-  function registrationHandler(event) {
-    event.preventDefault();
+  const emailInputRef = useRef();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
-  }
+  const registrationHandler = async (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const response = await fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <section className={classes.newsletter}>
@@ -15,10 +23,11 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
---
+++ next-events-app/pages/api/newsletter.js
@@ -0,0 +1,14 @@
+const handler = (req, res) => {
+  if (req.method === 'POST') {
+    const userEmail = req.body.email;
+    if (!userEmail || !userEmail.includes('@')) {
+      console.log(userEmail);
+      return res.status(422).json({ message: 'Invalid email address.' });
+    }
+
+    console.log(userEmail);
+    return res.status(201).json({ message: 'Signed up!' });
+  }
+};
+
+export default handler;
