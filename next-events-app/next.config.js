9eed951ff8b5c3362809c66a486e91321a7e19e1
DarrelA <darrelaiscoding@gmail.com>
2023-04-23 17:11:49 +0800

    Connecting to MongoDB

--- next-events-app/next.config.js
+++ next-events-app/next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     firebaseURL:
       'https://nextjs-d69a4-default-rtdb.asia-southeast1.firebasedatabase.app/events.json',
   },
+  serverRuntimeConfig: {
+    mongoConnectionString:
+      'mongodb+srv://user1:Y7pRbuTy5BR05V97@cluster0.b2j2h.mongodb.net/newsletter?retryWrites=true&w=majority',
+  },
 };
 
 module.exports = nextConfig;
--- next-events-app/pages/api/newsletter.js
+++ next-events-app/pages/api/newsletter.js
@@ -1,4 +1,7 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+import getConfig from 'next/config';
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
     if (!userEmail || !userEmail.includes('@')) {
@@ -6,8 +9,18 @@ const handler = (req, res) => {
       return res.status(422).json({ message: 'Invalid email address.' });
     }
 
-    console.log(userEmail);
-    return res.status(201).json({ message: 'Signed up!' });
+    const { serverRuntimeConfig } = getConfig();
+    const mongoConnectionString = serverRuntimeConfig.mongoConnectionString;
+
+    try {
+      const client = await MongoClient.connect(mongoConnectionString);
+      const db = client.db();
+      await db.collection('emails').insertOne({ email: userEmail });
+      client.close();
+      return res.status(201).json({ message: 'Signed up!' });
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
 
