acb49a744ca98f780b3c91fd408a45ee33331b9c
DarrelA <darrelaiscoding@gmail.com>
2023-04-23 15:32:19 +0800

    `GET` request for comments

--- next-events-app/components/input/comments.js
+++ next-events-app/components/input/comments.js
@@ -1,21 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
-import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NewComment from './new-comment';
 
 function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const getComments = async () => {
+      const response = await fetch(`/api/comments/${eventId}`);
+      const data = await response.json();
+      console.log(data);
+      setComments(data.comment);
+    };
+
+    if (showComments) getComments();
+  }, [showComments]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
-  function addCommentHandler(commentData) {
-    // send data to API
-  }
+  const addCommentHandler = async (commentData) => {
+    const response = await fetch(`/api/comments/${eventId}`, {
+      method: 'POST',
+      body: JSON.stringify(commentData),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <section className={classes.comments}>
@@ -23,7 +41,7 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList items={comments} />}
     </section>
   );
 }
---
+++ next-events-app/pages/api/comments/[eventId].js
@@ -0,0 +1,36 @@
+const handler = (req, res) => {
+  const eventId = req.query.eventId;
+
+  if (req.method === 'POST') {
+    const { email, name, text } = req.body;
+    if (
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !text ||
+      text.trim() === ''
+    )
+      return res.status(422).json({ message: 'Invalid input.' });
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+
+    console.log(newComment);
+    return res.status(201).json({ message: 'Added comment.', comment: newComment });
+  }
+
+  if (req.method === 'GET') {
+    const dummyList = [
+      { id: 'c1', name: 'Max', text: 'Comment1' },
+      { id: 'c2', name: 'Manu', text: 'Comment2' },
+    ];
+
+    return res.status(200).json({ comment: dummyList });
+  }
+};
+
+export default handler;
