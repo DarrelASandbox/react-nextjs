7acb119cddc9e05efeb2cf3c6c28dc168782401b
DarrelA <darrelaiscoding@gmail.com>
2023-05-01 22:08:20 +0800

    `POST` API route for `ContactForm` component

---
+++ next-full-blog/pages/api/contact.js
@@ -0,0 +1,20 @@
+const handler = (req, res) => {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    )
+      return res.status(422).json({ message: 'Invalid input' });
+
+    const newMessage = { email, name, message };
+    console.log(newMessage);
+    return res.status(201).json({ message: 'Successfully stored message.' });
+  }
+};
+
+export default handler;
