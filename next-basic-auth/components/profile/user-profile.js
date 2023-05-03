021be399d6066c4c4e785886eae2f8056c509a65
DarrelA <darrelaiscoding@gmail.com>
2023-05-04 01:39:23 +0800

    Adding client-side page guard for route protection

--- next-basic-auth/components/profile/user-profile.js
+++ next-basic-auth/components/profile/user-profile.js
@@ -1,8 +1,14 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const router = useRouter();
+  const { status } = useSession();
+
+  if (status === 'loading') return <p className={classes.profile}>Loading...</p>;
+  if (status === 'unauthenticated') router.push('/auth');
 
   return (
     <section className={classes.profile}>
--- next-basic-auth/pages/auth.js
+++ next-basic-auth/pages/auth.js
@@ -1,6 +1,12 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import AuthForm from '../components/auth/auth-form';
 
 function AuthPage() {
+  const router = useRouter();
+  const { status } = useSession();
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'authenticated') router.push('/profile');
   return <AuthForm />;
 }
 
